d4d6308f9dd9130fc9770c32316da9db76a6b3cd
schddan <danielschmitzsc@gmail.com>
2024-10-21 09:48:38 -0300

    Rota de registro e mobile básico

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import Express from "express"
 
 const app = Express()
+app.use(Express.json())
 
 app.get('/pegar', function(req, res){
     res.send('Enviar esta mensagem')
@@ -8,6 +9,21 @@ app.get('/pegar', function(req, res){
 
 app.get('/pegaroutracoisa', function(req,res){
     res.send("Outra mensagem")
+})
+
+app.post('/registro', function(req, res){
+    try{
+        const {nome, sobrenome, email, senha, nascimento} = req.body
+        if(!nome || !sobrenome || !email || !senha || !nascimento){
+            res.status(406).send("Todos os campos devem ser enviados")
+            return
+        } 
+
+        res.status(201).send("Usuário criado")
+        return
+    } catch(error){
+        res.send("OK")
+    }
 })
 
 app.listen(8000)
